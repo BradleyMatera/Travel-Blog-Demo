73fa00d437a1052e619b00be5b88d6c07ef2fab3
MateraBradley-FS-1 <162147754+BradleyMatera@users.noreply.github.com>
2024-05-29 21:18:26 -0500

    move

---
+++ readysettravel/src/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    // Your state here
+  },
+  mutations: {
+    // Your mutations here
+  },
+  actions: {
+    // Your actions here
+  },
+  modules: {
+    // Your modules here
+  },
+});
