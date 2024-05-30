edea9993025bed774d6471c50b3414b6ed51fcf9
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2024-05-30 18:23:38 -0500

    fix

--- dev/readysettravel/tailwind.config.js
+++ dev/readysettravel/tailwind.config.js
@@ -1,15 +1,11 @@
-// tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
-  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      boxShadow: {
-        'outline-orange': '0 0 0 3px rgba(255, 165, 0, 0.5)', // Custom shadow class definition
-      },
-    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
