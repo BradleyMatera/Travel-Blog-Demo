625e258c5054a7e5de27ed4fad2bf21cd2102937
MateraBradley-FS-1 <162147754+BradleyMatera@users.noreply.github.com>
2024-05-29 21:51:07 -0500

    yes

--- readysettravel/src/index.js
+++ readysettravel/src/index.js
@@ -1,38 +1,10 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import axios from 'axios';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  state: {
-    tours: [],
-    destinations: [],
-    blogPosts: []
-  },
-  mutations: {
-    setTours(state, tours) {
-      state.tours = tours;
-    },
-    setDestinations(state, destinations) {
-      state.destinations = destinations;
-    },
-    setBlogPosts(state, blogPosts) {
-      state.blogPosts = blogPosts;
-    }
-  },
-  actions: {
-    async fetchTours({ commit }) {
-      const response = await axios.get('/api/tours');
-      commit('setTours', response.data);
-    },
-    async fetchDestinations({ commit }) {
-      const response = await axios.get('/api/destinations');
-      commit('setDestinations', response.data);
-    },
-    async fetchBlogPosts({ commit }) {
-      const response = await axios.get('/api/blog-posts');
-      commit('setBlogPosts', response.data);
-    }
-  }
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
