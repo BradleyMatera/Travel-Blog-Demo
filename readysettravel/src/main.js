ff7861c77203606e733392db1954c8b01c8408e5
MateraBradley-FS-1 <162147754+BradleyMatera@users.noreply.github.com>
2024-05-29 21:55:53 -0500

    fix

--- readysettravel/src/main.js
+++ readysettravel/src/main.js
@@ -1,12 +1,5 @@
-import Vue from 'vue';
-import App from '../components/App.vue';
-import router from './router';
-import store from '../store/index';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app');
+ReactDOM.render(<App />, document.getElementById('root'));
