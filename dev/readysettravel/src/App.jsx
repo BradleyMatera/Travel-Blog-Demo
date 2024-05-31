f0588dd0985f9362d8f09858b47276068e989b56
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2024-05-30 19:24:05 -0500

    1

--- dev/readysettravel/src/App.jsx
+++ dev/readysettravel/src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Destinations from './pages/Destinations';
-import Blog from './pages/Blog';
-import BookingForm from './pages/BookingForm';
+import Home from './components/Home';
+import About from './components/About';
+import Destinations from './components/Destinations';
+import Blog from './components//Blog';
+import BookingForm from './components/BookingForm';
 
 function App() {
   return (
